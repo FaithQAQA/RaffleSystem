26ffd332b24d79fd658684707aab2d5d1b63d30b
jalani <145871113+FaithQAQA@users.noreply.github.com>
2025-03-06 20:16:59 -0500

    Backend Url and function
    Updated to correct Backend Url and added new help function to add authorization header

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+
+  private baseUrl = 'https://backendserver-euba.onrender.com/api'; //backend base URL
+
+
+
+  constructor(private http: HttpClient) {}
+
+    // Helper function to add authorization header
+    private getHeaders(): HttpHeaders {
+      const token = localStorage.getItem('token'); // Getting token from local storage
+      return new HttpHeaders({
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      });
+    }
 }
