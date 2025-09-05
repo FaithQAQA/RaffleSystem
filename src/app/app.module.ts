a5e9d41e2214800b808f5250e0caca2440a3a33b
jalani <145871113+FaithQAQA@users.noreply.github.com>
2025-09-04 23:28:18 -0400

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
-import { SwiperModule } from 'swiper/types';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { NotificationPopoverComponent } from './components/notification-popover/notification-popover.component';
 @NgModule({
   declarations: [AppComponent,NotificationPopoverComponent],
-  
+
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, FormsModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
